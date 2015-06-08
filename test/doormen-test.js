17e02f239362fe0890b750b18c9b8ed73d159c87
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-06-08 16:52:17 +0200

    basic types: OK!

--- test/doormen-test.js
+++ test/doormen-test.js
@@ -36,31 +36,61 @@ describe( "Assertion utilities" , function() {
 	
 	it( "doormen.shouldThrow() should throw if the callback has not throw, and catch if it has throw" , function() {
 		
-		var throwed ;
+		var thrown ;
 		
 		
-		throwed = false ;
+		thrown = false ;
 		
 		try {
 			doormen.shouldThrow( function() {} ) ;
 		}
 		catch ( error ) {
-			throwed = true ;
+			thrown = true ;
 		}
 		
-		if ( ! throwed ) { throw new Error( 'Has not thrown' ) ; }
+		if ( ! thrown ) { throw new Error( 'It should throw!' ) ; }
 		
 		
-		throwed = false ;
+		thrown = false ;
 		
 		try {
 			doormen.shouldThrow( function() { throw new Error( 'Fatal error' ) ; } ) ;
 		}
 		catch ( error ) {
-			throwed = true ;
+			thrown = true ;
 		}
 		
-		if ( throwed ) { throw new Error( 'Has thrown' ) ; }
+		if ( thrown ) { throw new Error( 'It should *NOT* throw' ) ; }
+		
+	} ) ;
+	
+	it( "doormen.not() should throw if the data validate, and catch if it has throw" , function() {
+		
+		var thrown ;
+		
+		
+		thrown = false ;
+		
+		try {
+			doormen.not( 'text' , { type: 'string' } ) ;
+		}
+		catch ( error ) {
+			thrown = true ;
+		}
+		
+		if ( ! thrown ) { throw new Error( 'It should throw' ) ; }
+		
+		
+		thrown = false ;
+		
+		try {
+			doormen.not( 1 , { type: 'string' } ) ;
+		}
+		catch ( error ) {
+			thrown = true ;
+		}
+		
+		if ( thrown ) { throw new Error( 'It should *NOT* throw' ) ; }
 		
 	} ) ;
 } ) ;
@@ -69,9 +99,127 @@ describe( "Assertion utilities" , function() {
 
 describe( "Basic types" , function() {
 	
-	it( "should validate string" , function() {
-		doormen( 'toto' , { type: 'string' } ) ;
+	it( "should validate undefined accordingly" , function() {
+		doormen( undefined , { type: 'undefined' } ) ;
+		doormen.not( null , { type: 'undefined' } ) ;
+		doormen.not( false , { type: 'undefined' } ) ;
+		doormen.not( true , { type: 'undefined' } ) ;
+		doormen.not( 0 , { type: 'undefined' } ) ;
+		doormen.not( 1 , { type: 'undefined' } ) ;
+		doormen.not( '' , { type: 'undefined' } ) ;
+		doormen.not( 'text' , { type: 'undefined' } ) ;
+		doormen.not( {} , { type: 'undefined' } ) ;
+		doormen.not( [] , { type: 'undefined' } ) ;
+	} ) ;
+	
+	it( "should validate null accordingly" , function() {
+		doormen.not( undefined , { type: 'null' } ) ;
+		doormen( null , { type: 'null' } ) ;
+		doormen.not( false , { type: 'null' } ) ;
+		doormen.not( true , { type: 'null' } ) ;
+		doormen.not( 0 , { type: 'null' } ) ;
+		doormen.not( 1 , { type: 'null' } ) ;
+		doormen.not( '' , { type: 'null' } ) ;
+		doormen.not( 'text' , { type: 'null' } ) ;
+		doormen.not( {} , { type: 'null' } ) ;
+		doormen.not( [] , { type: 'null' } ) ;
+	} ) ;
+	
+	it( "should validate boolean accordingly" , function() {
+		doormen.not( undefined , { type: 'boolean' } ) ;
+		doormen.not( null , { type: 'boolean' } ) ;
+		doormen( false , { type: 'boolean' } ) ;
+		doormen( true , { type: 'boolean' } ) ;
+		doormen.not( 0 , { type: 'boolean' } ) ;
+		doormen.not( 1 , { type: 'boolean' } ) ;
+		doormen.not( '' , { type: 'boolean' } ) ;
+		doormen.not( 'text' , { type: 'boolean' } ) ;
+		doormen.not( {} , { type: 'boolean' } ) ;
+		doormen.not( [] , { type: 'boolean' } ) ;
+	} ) ;
+	
+	it( "should validate number accordingly" , function() {
+		doormen.not( undefined , { type: 'number' } ) ;
+		doormen.not( null , { type: 'number' } ) ;
+		doormen.not( false , { type: 'number' } ) ;
+		doormen.not( true , { type: 'number' } ) ;
+		doormen( 0 , { type: 'number' } ) ;
+		doormen( 1 , { type: 'number' } ) ;
+		doormen( Infinity , { type: 'number' } ) ;
+		doormen( NaN , { type: 'number' } ) ;
+		doormen.not( '' , { type: 'number' } ) ;
+		doormen.not( 'text' , { type: 'number' } ) ;
+		doormen.not( {} , { type: 'number' } ) ;
+		doormen.not( [] , { type: 'number' } ) ;
+	} ) ;
+	
+	it( "should validate string accordingly" , function() {
+		doormen.not( undefined , { type: 'string' } ) ;
+		doormen.not( null , { type: 'string' } ) ;
+		doormen.not( false , { type: 'string' } ) ;
+		doormen.not( true , { type: 'string' } ) ;
+		doormen.not( 0 , { type: 'string' } ) ;
 		doormen.not( 1 , { type: 'string' } ) ;
+		doormen( '' , { type: 'string' } ) ;
+		doormen( 'text' , { type: 'string' } ) ;
+		doormen.not( {} , { type: 'string' } ) ;
+		doormen.not( [] , { type: 'string' } ) ;
+	} ) ;
+	
+	it( "should validate object accordingly" , function() {
+		doormen.not( undefined , { type: 'object' } ) ;
+		doormen.not( null , { type: 'object' } ) ;
+		doormen.not( false , { type: 'object' } ) ;
+		doormen.not( true , { type: 'object' } ) ;
+		doormen.not( 0 , { type: 'object' } ) ;
+		doormen.not( 1 , { type: 'object' } ) ;
+		doormen.not( '' , { type: 'object' } ) ;
+		doormen.not( 'text' , { type: 'object' } ) ;
+		doormen( {} , { type: 'object' } ) ;
+		doormen( [] , { type: 'object' } ) ;
+		doormen.not( function(){} , { type: 'object' } ) ;
+	} ) ;
+	
+	it( "should validate function accordingly" , function() {
+		doormen.not( undefined , { type: 'function' } ) ;
+		doormen.not( null , { type: 'function' } ) ;
+		doormen.not( false , { type: 'function' } ) ;
+		doormen.not( true , { type: 'function' } ) ;
+		doormen.not( 0 , { type: 'function' } ) ;
+		doormen.not( 1 , { type: 'function' } ) ;
+		doormen.not( '' , { type: 'function' } ) ;
+		doormen.not( 'text' , { type: 'function' } ) ;
+		doormen.not( {} , { type: 'function' } ) ;
+		doormen.not( [] , { type: 'function' } ) ;
+		doormen( function(){} , { type: 'function' } ) ;
 	} ) ;
 } ) ;
+
+
+
+describe( "Numbers meta types" , function() {
+	
+	it( "should validate realNumber accordingly" , function() {
+		doormen( 0 , { type: 'realNumber' } ) ;
+		doormen( 1 , { type: 'realNumber' } ) ;
+		doormen( -1 , { type: 'realNumber' } ) ;
+		doormen( 0.3 , { type: 'realNumber' } ) ;
+		doormen( 18.36 , { type: 'realNumber' } ) ;
+		doormen.not( 1/0 , { type: 'realNumber' } ) ;
+		doormen.not( Infinity , { type: 'realNumber' } ) ;
+		doormen.not( -Infinity , { type: 'realNumber' } ) ;
+		doormen.not( NaN , { type: 'realNumber' } ) ;
+		
+		doormen.not( undefined , { type: 'realNumber' } ) ;
+		doormen.not( null , { type: 'realNumber' } ) ;
+		doormen.not( false , { type: 'realNumber' } ) ;
+		doormen.not( true , { type: 'realNumber' } ) ;
+		doormen.not( '' , { type: 'realNumber' } ) ;
+		doormen.not( 'text' , { type: 'realNumber' } ) ;
+		doormen.not( {} , { type: 'realNumber' } ) ;
+		doormen.not( [] , { type: 'realNumber' } ) ;
+	} ) ;
+} ) ;
+
+
 
