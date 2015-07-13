befc40aa01d4612ec6e1438013d009858568a63c
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-07-13 14:12:09 +0200

    remove restQuery specifics, add extend functions for typeChecker, sanitizer and filter

--- test/doormen-test.js
+++ test/doormen-test.js
@@ -1920,22 +1920,6 @@ describe( "Misc" , function() {
 		} ) ;
 	} ) ;
 } ) ;
-	
 
-
-describe( "restQuery specific" , function() {
-	
-	it( "restQuerySlug type checker" , function() {
-		
-		doormen( { type: 'restQuerySlug' } , 'my-slug' ) ;
-		doormen.not( { type: 'restQuerySlug' } , 'Not-a-slug' ) ;
-	} ) ;
-	
-	it( "restQuerySlug type checker" , function() {
-		
-		var slug = doormen( { type: 'restQuerySlug' , sanitize: 'restQueryRandomSlug' } , null ) ;
-		//console.log( slug ) ;
-	} ) ;
-} ) ;
 
 
